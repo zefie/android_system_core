Herrie82/android_system_core-1

Make adb use a custom prop for adb root
Change-Id: Ie2b8c5e9d75371da43305f003607869370bbd1a4

Former-commit-id: 9d97be460f473ec27a9090f9dda3b1768147b615

adb/daemon/main.cpp
@@ -78,7 +78,7 @@ static bool should_drop_privileges() {
     bool drop = ro_secure;
 
     // ... except "adb root" lets you keep privileges in a debuggable build.
-    std::string prop = android::base::GetProperty("service.adb.root", "");
+    std::string prop = android::base::GetProperty("lineage.service.adb.root", "");
     bool adb_root = (prop == "1");
     bool adb_unroot = (prop == "0");
     if (ro_debuggable && adb_root) {

adb/services.cpp
@@ -84,7 +84,7 @@ void restart_root_service(int fd, void *cookie) {
             return;
         }
 
-        android::base::SetProperty("service.adb.root", "1");
+        android::base::SetProperty("lineage.service.adb.root", "1");
         WriteFdExactly(fd, "restarting adbd as root\n");
         adb_close(fd);
     }
@@ -95,7 +95,7 @@ void restart_unroot_service(int fd, void *cookie) {
         WriteFdExactly(fd, "adbd not running as root\n");
         adb_close(fd);
     } else {
-        android::base::SetProperty("service.adb.root", "0");
+        android::base::SetProperty("lineage.service.adb.root", "0");
         WriteFdExactly(fd, "restarting adbd as non root\n");
         adb_close(fd);
     }

zefie/android_system_core

Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

adb/daemon/main.cpp
@@ -78,7 +78,7 @@ static bool should_drop_privileges() {
     bool drop = ro_secure;
 
     // ... except "adb root" lets you keep privileges in a debuggable build.
-    std::string prop = android::base::GetProperty("service.adb.root", "");
+    std::string prop = android::base::GetProperty("havoc.service.adb.root", "");
     bool adb_root = (prop == "1");
     bool adb_unroot = (prop == "0");
     if (ro_debuggable && adb_root) {

adb/services.cpp
@@ -84,7 +84,7 @@ void restart_root_service(int fd, void *cookie) {
             return;
         }
 
-        android::base::SetProperty("service.adb.root", "1");
+        android::base::SetProperty("havoc.service.adb.root", "1");
         WriteFdExactly(fd, "restarting adbd as root\n");
         adb_close(fd);
     }
@@ -95,7 +95,7 @@ void restart_unroot_service(int fd, void *cookie) {
         WriteFdExactly(fd, "adbd not running as root\n");
         adb_close(fd);
     } else {
-        android::base::SetProperty("service.adb.root", "0");
+        android::base::SetProperty("havoc.service.adb.root", "0");
         WriteFdExactly(fd, "restarting adbd as non root\n");
         adb_close(fd);
     }